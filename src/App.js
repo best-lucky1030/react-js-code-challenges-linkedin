647d4bcab332e764a20e555a7bb9f2f50eef4122
best-lucky1030 <bestlucky1030@gmail.com>
2022-12-01 17:22:40 +0900

    add dog pics

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './index.css';
 import ColorRenderer from './01-color-renderer/ColorRenderer';
 import DarkMode from './02-dark-mode/DarkMode';
 import FormValidator from './03-form-validator/FormValidator';
+import DogPics from './04-dog-pics/DogPics';
 
 function App() {
   return (
     <div className="App">
       {/* <ColorRenderer /> */}
       {/* <DarkMode /> */}
-      <FormValidator />
+      {/* <FormValidator /> */}
+      <DogPics />
     </div>
   );
 }
